c3b9ef53849c1db3f5ebf83a92fa60c1d635815b
Vasily Gudoshnikov <vgoodvin@gmail.com>
2013-12-03 19:41:48 +0400

    Refactored JsTrans.php to increase code readability.

--- JsTrans.php
+++ JsTrans.php
@@ -41,43 +41,74 @@ class JsTrans extends CApplicationComponent
         $dictionaryFile = "dictionary-{$hash}.js";
         // Generate dictionary file if not exists or YII DEBUG is set
         if (!file_exists($basePath . '/' . $dictionaryFile) || YII_DEBUG) {
-            // Declare config (passed to JS)
-            $config = array('language' => $this->defaultLanguage);
-            // Loop message files and store translations in array
-            $dictionary = array();
-            foreach ($this->languages as $lang) {
-                if (!isset($dictionary[$lang])) {
-                    $dictionary[$lang] = array();
-                }
-                foreach ($this->categories as $cat) {
-                    if (Yii::app()->messages instanceof CDbMessageSource) {
-                        // Read dictionary from the database.
-                        $tbl_source = Yii::app()->messages->sourceMessageTable;
-                        $tbl_message = Yii::app()->messages->translatedMessageTable;
-                        $command = db()->createCommand()->select('s.message, m.translation')
-                            ->from("{$tbl_source} s")->join("{$tbl_message} m", 'm.source_id = s.id')
-                            ->where('s.category = :cat AND m.language = :lang', array(':cat' => $cat, ':lang' => $lang))
-                            ->query();
-                        while ($row = $command->read()) {
-                            $dictionary[$lang][$cat][$row['message']] = $row['translation'];
-                        }
-                    } elseif (Yii::app()->messages instanceof CPhpMessageSource) {
-                        // Read dictionary from PHP-files.
-                        $messagesFolder = rtrim(Yii::app()->messages->basePath, '\/');
-                        $messageFile = $messagesFolder . '/' . $lang . '/' . $cat . '.php';
-                        if (file_exists($messageFile)) $dictionary[$lang][$cat] = array_filter(require($messageFile));
-                    }
-                }
-            }
-            // Save config/dictionary
-            $data = 'Yii.translate.config=' . CJSON::encode($config) . ';';
-            $data .= 'Yii.translate.dictionary=' . CJSON::encode($dictionary);
-            // Save to dictionary file
-            if (!file_put_contents($basePath . '/' . $dictionaryFile, $data)) {
-                Yii::log('Error: Could not write dictionary file, check file permissions', 'trace', 'jstrans');
-            }
+            $this->generateDictionaryFile($basePath, $dictionaryFile);
         }
         // Publish library and dictionary
+        $this->publishScripts($basePath, $baseUrl, $dictionaryFile);
+    }
+
+    private function generateDictionaryFile($basePath, $dictionaryFile)
+    {
+        // Loop message files and store translations in array
+        $dictionary = array();
+        foreach ($this->languages as $lang) {
+            if (!isset($dictionary[$lang])) {
+                $dictionary[$lang] = array();
+            }
+            foreach ($this->categories as $cat) {
+                if (Yii::app()->messages instanceof CDbMessageSource) {
+                    $this->readDictionaryFromDb($cat, $lang, $dictionary);
+                } elseif (Yii::app()->messages instanceof CPhpMessageSource) {
+                    $this->readDictionaryFromFile($cat, $lang, $dictionary);
+                }
+            }
+        }
+        // Save config/dictionary
+        $config = array('language' => $this->defaultLanguage);
+        $data = 'Yii.translate.config=' . CJSON::encode($config) . ';';
+        $data .= 'Yii.translate.dictionary=' . CJSON::encode($dictionary);
+        // Save to dictionary file
+        if (!file_put_contents($basePath . '/' . $dictionaryFile, $data)) {
+            Yii::log('Error: Could not write dictionary file, check file permissions', 'trace', 'jstrans');
+        }
+    }
+
+    /**
+     * Reads dictionary from the database.
+     * @param $cat
+     * @param $lang
+     * @param $dictionary
+     */
+    private function readDictionaryFromDb($cat, $lang, &$dictionary)
+    {
+        $tbl_source = Yii::app()->messages->sourceMessageTable;
+        $tbl_message = Yii::app()->messages->translatedMessageTable;
+        $command = db()->createCommand()->select('s.message, m.translation')
+            ->from("{$tbl_source} s")->join("{$tbl_message} m", 'm.source_id = s.id')
+            ->where('s.category = :cat AND m.language = :lang', array(':cat' => $cat, ':lang' => $lang))
+            ->query();
+        while ($row = $command->read()) {
+            $dictionary[$lang][$cat][$row['message']] = $row['translation'];
+        }
+    }
+
+    /**
+     * Reads dictionary from PHP-files.
+     * @param $cat
+     * @param $lang
+     * @param $dictionary
+     */
+    private function readDictionaryFromFile($cat, $lang, &$dictionary)
+    {
+        $messagesFolder = rtrim(Yii::app()->messages->basePath, '\/');
+        $messageFile = $messagesFolder . '/' . $lang . '/' . $cat . '.php';
+        if (file_exists($messageFile)) {
+            $dictionary[$lang][$cat] = array_filter(require($messageFile));
+        }
+    }
+
+    private function publishScripts($basePath, $baseUrl, $dictionaryFile)
+    {
         if (file_exists($basePath . '/' . $dictionaryFile)) {
             Yii::app()->getClientScript()
                 ->registerScriptFile($baseUrl . '/JsTrans.min.js', CClientScript::POS_HEAD)
